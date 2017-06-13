093ea513055111fe982fabc30d97b7154f80551e
crianbra <crianbra@espol.edu.ec>
2017-06-13 11:41:39 -0500

    boton actividad 2

--- app/src/main/java/com/example/dptoredes/taller2/MainActivity.java
+++ app/src/main/java/com/example/dptoredes/taller2/MainActivity.java
@@ -1,16 +1,38 @@
 package com.example.dptoredes.taller2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    String tag = "Lifecycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("mensaje", "creando");
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d (tag,"Si llamo a la funcion");
+                Intent intent01 = new Intent(MainActivity.this, AcercaDe.class);
+                startActivity(intent01);
+
+            }
+        });/*OPCION PARA LLAMAR OTRA ACTIVIDAD*/
+    }
+
+    public void AbrirAcerca() {
+        Log.d (tag,"Si llamo a la funcion");
+        Intent intent = new Intent(this, AcercaDe.class);
+        startActivity(intent);
     }
 
     @Override
@@ -42,5 +64,8 @@ public class MainActivity extends AppCompatActivity {
         Log.d("mensaje", "resume");
         super.onResume();
     }
+
+
+
 
 }
